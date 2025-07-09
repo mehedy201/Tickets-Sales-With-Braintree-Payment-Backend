1a203e8563f3a092c8be52df495635275a743892
mehedy201 <mehedy643@gmail.com>
2025-07-10 01:50:20 +0600

    update download api for download ticket rander freindly api

--- controlar/v1/ICGHC/makePayment.controlar.js
+++ controlar/v1/ICGHC/makePayment.controlar.js
@@ -270,7 +270,20 @@ module.exports.downloadPurcherDetails = async (req, res, next) => {
         
 
 
-        const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+        // const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+        const browser = await puppeteer.launch({
+        headless: 'new',
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process',
+          '--disable-gpu'
+        ]
+      });
         const page = await browser.newPage();
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
@@ -391,7 +404,20 @@ module.exports.downloadAttendeesTickets = async (req, res, next) => {
         });
 
 
-        const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+        // const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+        const browser = await puppeteer.launch({
+        headless: 'new',
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process',
+          '--disable-gpu'
+        ]
+      });
         const page = await browser.newPage();
         await page.setContent(html, { waitUntil: 'networkidle0' });
 
