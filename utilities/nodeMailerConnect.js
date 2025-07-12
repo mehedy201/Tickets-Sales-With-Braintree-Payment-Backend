50244df88819f955535e53c441fc28fc9bbd9a1a
mehedy201 <mehedy643@gmail.com>
2025-07-12 22:26:43 +0600

    chage sender email

--- utilities/nodeMailerConnect.js
+++ utilities/nodeMailerConnect.js
@@ -1,10 +1,10 @@
 const nodemailer = require("nodemailer");
 
 module.exports.transporter = nodemailer.createTransport({
-    // host: "smtp.hostinger.com",
-    service: 'gmail',
-    port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports if false 587 port
+    host: "smtp.hostinger.com",
+    // service: 'gmail',
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports if false 587 port
     auth: {
       user: process.env.NODE_MAILER_USER_EMAIL,// hostinger email
       pass: process.env.NODE_MAILER_PASSWORD, // hostinger Email password
